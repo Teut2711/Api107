8f0236361cd07b8184ae9268cfe0d59c391ede9a
Vishesh Mangla <manglavishesh64@gmail.com>
2022-01-28 05:31:11 +0530

    project started

---
+++ server.js
@@ -0,0 +1,46 @@
+const fastify = require('fastify')({ logger: true })
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+fastify.register(require('fastify-mongodb'), {
+  forceClose: true,
+  url: process.env.CONNECT_DB,
+})
+
+fastify.route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    // request needs to have a querystring with a `name` parameter
+    querystring: {
+      name: { type: 'string' }
+    },
+    // the response needs to be an object with an `hello` property of type 'string'
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' }
+        }
+      }
+    }
+  },
+  // this function is executed for every request before the handler is executed
+  preHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world' }
+  }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
